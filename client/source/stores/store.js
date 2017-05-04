3d48a68c0427dc957d5aa5f6246e8eedf4d44bdc
matsutani <yushiro.ma2ta2.21@gmail.com>
2017-05-04 15:03:41 +0900

    webpackのれん酒

--- client/source/stores/store.js
+++ client/source/stores/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import measureMiddleware from '../middlewares/measure';
-import rootReducer from '../reducers/index';
+import rootReducer from '../reducers/index';
 import { getTodos, setTodos } from '../actions'
 
 const store = createStore(
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :sample, only: [:index]
+  resources :kanban, only: [:index]
 
   resources :todos do
     get 'toggle'
